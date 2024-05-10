e5182ee0e6b8cdbf7cc131cf1adb0fc40a52011d
eatradish <sakiiily@aosc.io>
2024-05-10 17:20:38 +0800

    feat(frontend): improve `/package` style

--- frontend/src/views/PackageView.vue
+++ frontend/src/views/PackageView.vue
@@ -9,20 +9,44 @@
     <el-form-item label="Version">
       {{ version }}
     </el-form-item>
-    <el-form-item label="deps">
-      {{ deps }}
+    <el-form-item label="Deps">
+      <div class="links">
+        <li v-for="d of deps" :key="d">
+          <el-link :href="'../package/' + d">{{ d }}</el-link>
+        </li>
+      </div>
     </el-form-item>
     <el-form-item label="Build Deps">
-      {{ buildDeps }}
+      <div class="links">
+        <li v-for="d of buildDeps" :key="d">
+          <el-link :href="'../package/' + d">{{ d }}</el-link>
+        </li>
+      </div>
     </el-form-item>
     <el-form-item label="Pkg Breaks">
-      {{ pkgBreaks }}
+      <div class="break-links">
+        <li v-for="d of pkgBreaks" :key="d">
+          <div class="breaks">
+            <el-link :href="'../package/' + d.name">{{ d.name }}</el-link>
+            <span>{{ d.comp }}</span>
+            <span class="version">{{ d.version }}</span>
+          </div>
+        </li>
+      </div>
     </el-form-item>
     <el-form-item label="Pkg Recommend">
-      {{ pkgBreaks }}
+      <div class="links">
+        <li v-for="d of pkgRecommend" :key="d">
+          <el-link :href="'../package/' + d">{{ d }}</el-link>
+        </li>
+      </div>
     </el-form-item>
     <el-form-item label="Pkg Provides">
-      {{ provides }}
+      <div class="links">
+        <li v-for="d of provides" :key="d">
+          <el-link :href="'../package/' + d">{{ d }}</el-link>
+        </li>
+      </div>
     </el-form-item>
   </el-form>
 </template>
@@ -38,23 +62,56 @@ export default {
       buildDeps: [],
       pkgBreaks: [],
       pkgRecommend: [],
-      provides: [],
+      provides: []
     }
   },
   props: {
-    name: String,
+    name: String
   },
   async mounted() {
-    const resp = await fetch(`http://127.0.0.1:2333/package?name=${this.name}`);
-    const data = await resp.json();
-    this.pkgName = data.name;
-    this.version = data.version;
-    this.desc = data.desc;
-    this.deps = data.deps;
-    this.buildDeps = data.build_deps;
-    this.pkgBreaks = data.pkgbreak;
-    this.pkgRecommend = data.pkgrecom;
-    this.provides = data.provides;
+    const resp = await fetch(`http://127.0.0.1:2333/package?name=${this.name}`)
+    const data = await resp.json()
+    this.pkgName = data.name
+    this.version = data.version
+    this.desc = data.desc
+    this.deps = data.deps
+    this.buildDeps = data.build_deps
+    this.pkgBreaks = data.pkgbreak
+    this.pkgRecommend = data.pkgrecom
+    this.provides = data.provides
   }
 }
 </script>
+
+<style>
+.links {
+  list-style: none;
+  display: flex;
+  flex-flow: column;
+  white-space: normal;
+  word-break: keep-all;
+  max-width: 300px;
+}
+
+.break-links {
+  list-style: none;
+  display: flex;
+  flex-flow: column;
+  white-space: normal;
+  word-break: keep-all;
+}
+
+.el-link {
+  margin-right: 8px;
+}
+
+.breaks {
+  display: flex;
+  flex-flow: row;
+  word-break: keep-all;
+}
+
+.version {
+  margin-left: 6px;
+}
+</style>
